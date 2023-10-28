8dd7806f77ff3cbf125bab6777460c605d4659fc
victor-villca <villcavictorleon@gmail.com>
2023-10-28 16:48:57 -0400

    node js environment init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app =express();
+const PORT = 5000;
+ 
+app.use(express.json());
+
+app.get("/", (req, res)=>res.send("Home"));
+
+app.listen(PORT, ()=>console.log(`Running on http://localhost:${PORT}`))
