3c64d44c96971e829c7ddbdf2fc743e0adc49cf0
victor-villca <villcavictorleon@gmail.com>
2023-10-28 19:15:43 -0400

    product rest api with database

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+import express from 'express'
+import mongoose from 'mongoose';
+import productRouter from './src/routes/Products.js';
+
 const app =express();
 const PORT = 5000;
  
 app.use(express.json());
 
-app.get("/", (req, res)=>res.send("Home"));
+app.use("/products", productRouter)
 
-app.listen(PORT, ()=>console.log(`Running on http://localhost:${PORT}`))
+mongoose.connect("mongodb+srv://admin:admin123@clusterapi.getb6wv.mongodb.net/Node-API?retryWrites=true&w=majority")
+.then( ()=>{
+    console.log("Succesfully connected to mongoDB");
+    app.listen(PORT, ()=>console.log(`Running on http://localhost:${PORT}`))
+}).catch((error) => {
+    console.log(error)
+})
