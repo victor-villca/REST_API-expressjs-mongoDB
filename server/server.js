c32eeaaaa7643a7ffae559a9b94bb161503e0fb3
victor-villca <villcavictorleon@gmail.com>
2023-10-31 15:03:37 -0400

    server refactorization

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import userRouter from './src/routes/Users.js';
 
 const app =express();
 const PORT = 5000;
- 
+
 app.use(express.json());
 app.use("/products", productRouter);
 app.use("/users", userRouter );
@@ -13,6 +13,7 @@ app.use("/users", userRouter );
 app.get('/', (req, res) => res.json({message: 'Ecommerce - Welcome'}));
 
 mongoose.connect("mongodb+srv://admin:admin123@clusterapi.getb6wv.mongodb.net/Node-API?retryWrites=true&w=majority")
+
 .then( ()=>{
     console.log("Succesfully connected to mongoDB");
     app.listen(PORT, ()=>console.log(`Running on http://localhost:${PORT}`))
